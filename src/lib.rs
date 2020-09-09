1ef60b96dd40c3e2ceba76d223e7299d67f1919b
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-09-09 18:25:43 +0530

    Add tauri_handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,35 @@
 pub mod db;
 
+use tauri::Webview;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Reply {
+  data: String,
+}
+
+pub fn blah() {
+  println!("Blah bleh bluh");
+}
+
+pub fn tauri_handler(webview: &mut Webview, source: String) {
+  let mut webview = webview.as_mut();
+
+  tauri::event::listen(String::from("js-event"), move |msg| {
+    println!("got js-event with message '{:?}'", msg);
+    let reply = Reply {
+      data: "something else".to_string(),
+    };
+
+    tauri::event::emit(
+      &mut webview,
+      String::from("rust-event"),
+      Some(serde_json::to_string(&reply).unwrap()),
+    )
+    .expect("failed to emit");
+  });
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
