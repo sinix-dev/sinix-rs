6c0814405e19293f754c41d6ca4dd892d859e334
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-09-11 16:42:04 +0530

    Cargo fmt

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,11 @@
 use pickledb::{PickleDb, PickleDbDumpPolicy, SerializationMethod};
 
 pub fn init(db_name: String) -> PickleDb {
-  let db = PickleDb::new(db_name, PickleDbDumpPolicy::AutoDump, SerializationMethod::Json);
+    let db = PickleDb::new(
+        db_name,
+        PickleDbDumpPolicy::AutoDump,
+        SerializationMethod::Json,
+    );
 
-  db
+    db
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,35 +2,43 @@ mod db;
 mod event_handlers;
 mod models;
 
+use dirs;
+use std::fs;
 use std::path::Path;
 use tauri::Webview;
-use std::fs;
-use dirs;
 
 pub fn init() {
-  let home_dir = dirs::home_dir().unwrap();
-  let games_dir = Path::new(&home_dir).join(".sinix/games").to_str().unwrap().to_string();
-  let data_dir = Path::new(&home_dir).join(".sinix/data").to_str().unwrap().to_string();
+    let home_dir = dirs::home_dir().unwrap();
+    let games_dir = Path::new(&home_dir)
+        .join(".sinix/games")
+        .to_str()
+        .unwrap()
+        .to_string();
+    let data_dir = Path::new(&home_dir)
+        .join(".sinix/data")
+        .to_str()
+        .unwrap()
+        .to_string();
 
-  fs::create_dir_all(&games_dir).unwrap();
-  fs::create_dir_all(&data_dir).unwrap();
+    fs::create_dir_all(&games_dir).unwrap();
+    fs::create_dir_all(&data_dir).unwrap();
 }
 
 pub fn tauri_handler(webview: &mut Webview, _source: String) {
-  // handler function for tauri app setup callback
+    // handler function for tauri app setup callback
 
-  let webview = webview.as_mut();
+    let webview = webview.as_mut();
 
-  tauri::event::listen(String::from("sinix-install"), move |msg| {
-    event_handlers::sinix_install(webview.clone(), msg)
-  });
+    tauri::event::listen(String::from("sinix-install"), move |msg| {
+        event_handlers::sinix_install(webview.clone(), msg)
+    });
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-      let mut db_instance = super::db::init(String::from("sanket143.db"));
-      db_instance.set("name", &"sanket").unwrap();
+        let mut db_instance = super::db::init(String::from("sanket143.db"));
+        db_instance.set("name", &"sanket").unwrap();
     }
 }
