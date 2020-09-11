702d8d085a6c7cbb2e25bb65cd8639ca032b9608
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-09-11 12:37:59 +0530

    Install Application

--- src/event_handlers.rs
+++ src/event_handlers.rs
@@ -1,7 +1,65 @@
 use crate::models::Reply;
+use std::fs;
+use std::io;
+use zip;
+
+fn extract_and_copy(file_name: String){
+  let file = fs::File::open(&file_name).unwrap();
+  let mut archive = zip::ZipArchive::new(file).unwrap();
+
+  for i in 0..archive.len() {
+    let mut file = archive.by_index(i).unwrap();
+    let outpath = file.sanitized_name();
+
+    {
+      let comment = file.comment();
+      if !comment.is_empty() {
+        println!("File {} comment: {}", i, comment);
+      }
+    }
+
+    if (&*file.name()).ends_with("/") {
+      println!(
+        "File {} extracted to \"{}\"",
+        i,
+        outpath.as_path().display()
+      );
+
+      fs::create_dir_all(&outpath).unwrap();
+    } else {
+      println!(
+        "File {} extracted to \"{}\" ({} bytes)",
+        i,
+        outpath.as_path().display(),
+        file.size()
+      );
+
+      if let Some(p) = outpath.parent() {
+        if !p.exists() {
+          fs::create_dir_all(&p).unwrap();
+        }
+      }
+
+      let mut outfile = fs::File::create(&outpath).unwrap();
+      io::copy(&mut file, &mut outfile).unwrap();
+    }
+
+    // Get and Set permissions
+    #[cfg(unix)]
+    {
+      use std::os::unix::fs::PermissionsExt;
+
+      if let Some(mode) = file.unix_mode() {
+        fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
+      }
+    }
+  }
+}
 
 pub fn sinix_install(mut webview: tauri::WebviewMut, msg: Option<String>){
-  let _file_name = msg.unwrap();
+  // Install a new app
+  let file_name = msg.unwrap();
+  extract_and_copy(file_name);
 
   let reply = Reply {
     data: "something else".to_string(),
